b5955492607c1c569a829f0b6de74a36608a3dd1
Mrchinnu7 <chinnanaik.cse.rymec@gmail.com>
2025-01-25 16:33:44 +0530

    commited request

--- src/main/java/com/example/bsm/request/HospitalRequest.java
+++ src/main/java/com/example/bsm/request/HospitalRequest.java
@@ -1,4 +1,15 @@
 package com.example.bsm.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class HospitalRequest {
+
+
+    private String hospitalName;
 }
--- src/main/java/com/example/bsm/request/SampleRequest.java
+++ src/main/java/com/example/bsm/request/SampleRequest.java
@@ -1,4 +1,21 @@
 package com.example.bsm.request;
 
+import com.example.bsm.enums.BloodGroup;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class SampleRequest {
+
+
+    private BloodGroup bloodGroup;
+    private int quantity;
+    private boolean availability;
+    private int emergencyUnits;
+    private int availableUnits;
 }
