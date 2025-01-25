03afc27ffcfbd744538ec6382457aed615fbc62d
Mrchinnu7 <chinnanaik.cse.rymec@gmail.com>
2025-01-25 16:34:03 +0530

    commited response

--- src/main/java/com/example/bsm/response/UserResponse.java
+++ src/main/java/com/example/bsm/response/UserResponse.java
@@ -1,4 +1,28 @@
 package com.example.bsm.response;
 
+import com.example.bsm.enums.BloodGroup;
+import com.example.bsm.enums.Gender;
+import com.example.bsm.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserResponse {
+
+    private int userId;
+    private String userName;
+    private BloodGroup bloodGroup;
+    private int age;
+    private Gender gender;
+    private String availableCity;
+    private String lastDonatedAt;
+    private String lastCreatedAt;
+    private String lastModifiedAt;
+    private Role role;
+
 }
