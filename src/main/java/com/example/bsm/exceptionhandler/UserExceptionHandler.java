da46063a4814a5707e1909e0db745fe584fdbc87
Mrchinnu7 <chinnanaik.cse.rymec@gmail.com>
2025-01-25 16:31:14 +0530

    commited exception handler

--- src/main/java/com/example/bsm/exceptionhandler/UserExceptionHandler.java
+++ src/main/java/com/example/bsm/exceptionhandler/UserExceptionHandler.java
@@ -1,4 +1,21 @@
 package com.example.bsm.exceptionhandler;
 
+import com.example.bsm.exception.UserNotFoundByIdException;
+import com.example.bsm.utility.ErrorStructure;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@AllArgsConstructor
 public class UserExceptionHandler {
+
+//    private final RestController responseBuilder;
+//
+//    public ResponseEntity<ErrorStructure<String>> handlerUserNotFoundById(UserNotFoundByIdException ex){
+//        return responseBuilder.er(HttpStatus.NOT_FOUND,ex.getMessage(),"User Not Found By Ud");
+//    }
+
 }
